40c9a163bd38a475ea0ed714b64463ecab52bcfd
Sam Bencivengo <sambencivengo@gmail.com>
2021-10-19 10:32:33 -0400

    commit tree b673357500c87640a0808a6e8a682f0faff2e17f

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,26 +1,46 @@
-import React from "react";
-import { v4 as uuid } from "uuid";
+import React from 'react';
+import { useState } from 'react/cjs/react.development';
+import { v4 as uuid } from 'uuid';
 
-function ItemForm(props) {
-  return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
+function ItemForm({ onItemFormSubmit }) {
+	const [newItem, setNewItem] = useState({
+		id: uuid(), // the `uuid` library can be used to generate a unique id
+		name: '',
+		category: 'Produce',
+	});
 
-      <label>
-        Category:
-        <select name="category">
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
+	const handleNewItemForm = (e) => {
+		setNewItem({ ...newItem, [e.target.name]: e.target.value });
+	};
 
-      <button type="submit">Add to List</button>
-    </form>
-  );
+	const handleAddItem = (e) => {
+		e.preventDefault();
+		onItemFormSubmit(newItem);
+	};
+
+	return (
+		<form
+			onSubmit={handleAddItem}
+			onChange={handleNewItemForm}
+			className="NewItem"
+		>
+			<label>
+				Name:
+				<input type="text" name="name" />
+			</label>
+
+			<label>
+				Category:
+				<select onChange={handleNewItemForm} name="category">
+					<option value="Produce">Produce</option>
+					<option value="Dairy">Dairy</option>
+					<option value="Dessert">Dessert</option>
+				</select>
+			</label>
+
+			<button type="submit">Add to List</button>
+		</form>
+	);
 }
 
 export default ItemForm;
